fa715f1fa1f77cf6c929c85ae5f4fa15bddba45d
latinusveridas <sparteclaudius749@gmail.com>
2019-06-28 15:15:45 +0200

    deployer

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,7 @@ development: {
 host: "localhost", 
 port: 8543,
 network_id: "*",
-from: "0xfa2361236b5ac8079cb6cf250e5284922ed9ba9a",
+from: "0xacc8de88f6e2435bac5978a43726a6ce9021092c",
 gas: 20000000
 }
 }
